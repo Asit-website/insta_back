bc3bd6dd848c5ca330a2beb7fc8356020cbade88
Manishrajwar <manishsinghrajwar80@gmail.com>
2024-11-18 18:32:08 +0530

    quotation update

--- models/Quatation/Quatation.js
+++ models/Quatation/Quatation.js
@@ -1,91 +1,42 @@
 import mongoose from "mongoose";
 
-
 const itemSchema = new mongoose.Schema({
-  description: {
-    type: String,
-  },
-  quantity: {
-    type: String,
-  },
-  price: {
-    type: String,
-  },
-  total: {
-    type: String,
-  }
+  description: { type: String },
+  quantity: { type: String },
+  price: { type: String },
+  total: { type: String },
 });
 
-const mySchema = new mongoose.Schema({
+const mySchema = new mongoose.Schema(
+  {
     UserId: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'User'
+      ref: "User",
     },
     leadId: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'Lead'
+      ref: "Lead",
     },
-    content:{
-      type:String, 
-    },
-    quotationNum:{
-      type:String ,
-    },
-    customerName:{
-      type:String , 
-    },
-    customerReq: {
-      type:String , 
-    },
-    mobileNum: {
-      type:String, 
-    },
-    quotationDate: {
-      type:String, 
-    },
-    validUntil:{
-      type:String , 
-    },
-    customerId:{
-      type:String, 
-    },
-    companyName: {
-      type:String , 
-    },
-    companyAddress: {
-      type:String , 
-    },
-    companyGSTIN:{
-      type:String , 
-    },
-    companyWebsite:{
-      type:String , 
-    },
-        items: [itemSchema]
+    customerAddress: { type: String },
+    customerCompany: { type: String },
+    customerName: { type: String },
+    quotationDate: { type: String },
+    quotationNum: { type: String },
+    rows: [
+      {
+        subject: { type: String, required: true }, 
+        para: [
+          {
+            type: mongoose.Schema.Types.Mixed, 
+          },
+        ],
+        items: [itemSchema], 
+      },
+    ],
+  },
+  { timestamps: true } 
+);
 
-  //   QuatationNo:String,
-  //   GstNo:String,
-  //   SacCode:String,
-  //   PlacedSupply:String,
-  //   BillTo:String,
-  //   ShipTo:String,
-  //   ClientName:String,
-  //   Address:String,
-  //   Mobile:Number,
-  //   Email:String,
-  //   ItemDescription:String,
-  //   Qty:String,
-  //   Price:String,
-  //   Amount:String,
-  //   BalanceAmount:String,
-  //   Note:String,
-  //   currency:String,
-  //   ts: {
-  //     type: String,
-  //     default: new Date().getTime()
-  // },
-  },{timestamps:true});
+const Quatation = mongoose.model("Quatation", mySchema);
 
-  const Quatation = mongoose.model("Quatation", mySchema);
-
-  export default Quatation;
+export default Quatation;
